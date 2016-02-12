3224471adeab89ad537fa53b4a99900b5fae5e78
Andy Li <aytli@live.com>
2016-02-12 16:01:38 -0500

    labview now recognizes more than one packet id, should use hex or decimal numbers in case

--- labview/labview test code/main.c
+++ labview/labview test code/main.c
@@ -5,7 +5,8 @@
 void main()
 {
     int i = 0;
-    int n = 33;
+    int n = 65;
+    int m = 97;
     setup_adc_ports(sAN0);
     setup_timer_3(T3_DISABLED|T3_DIV_BY_1);
     setup_timer_4(T4_DISABLED,0,1);
@@ -24,13 +25,34 @@ void main()
             delay_ms(10);
         }
         
-        if (n < 126)
+        if (n < 90)
         {
             n++;
         }
         else
         {
-            n = 33;
+            n = 65;
+        }
+        
+        
+        putc(0x69);
+        output_high(HEARTBEAT);
+        output_low(HEARTBEAT);
+        delay_ms(10);
+        
+        for (i = 0 ; i < 10 ; i++)
+        {
+            putc(m);
+            delay_ms(10);
+        }
+        
+        if (m < 122)
+        {
+            m++;
+        }
+        else
+        {
+            m = 97;
         }
     }
 }
