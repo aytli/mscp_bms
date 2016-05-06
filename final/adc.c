2a4b8548936e1163f9660ea8ce8a9785f0aa7237
Andy Li <aytli@live.com>
2016-05-06 17:14:49 -0400

    added lookup tables to correctly map adc channels

--- final/adc.c
+++ final/adc.c
@@ -19,6 +19,21 @@ typedef struct
     float          converted;
 } temperature_t;
 
+// ADC channels on PCB are not mapped in order
+// These two arrays are lookup tables to correctly map the channels
+
+// ADC1:
+// Channel number:   0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11
+// Cell number:     10,  9,  8,  7,  5,  4,  3,  2, 15, 14, 13, 12
+// ADC index:        7,  6,  5,  4,  3,  2,  1,  0, 11, 10,  9,  8
+static unsigned int8 g_channel_map1[12] = {7,6,5,4,3,2,1,0,11,10,9,8};
+
+// ADC2:
+// Channel number:  12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
+// Cell number:     20, 19, 18, 17, 22, 23, 24, 25, 27, 28, 29, 30
+// ADC index:       15, 14, 13, 12, 16, 17, 18, 19, 20, 21, 22, 23
+static unsigned int8 g_channel_map2[12] = {15,14,13,12,16,17,18,19,20,21,22,23};
+
 // Configures the ADS7952 to operate in Auto-1 Mode
 void ads7952_init(void)
 {
@@ -79,7 +94,7 @@ void ads7952_read_all_channels(temperature_t * adc)
         output_high(ADC1_SEL);
         
         ch = msb >> 4;
-        adc[ch].raw = ((0x0F & msb) << 8 ) | lsb;
+        adc[g_channel_map1[ch]].raw = ((0x0F & msb) << 8 ) | lsb;
     }
     
     for (i = 12 ; i < 24 ; i ++)
@@ -99,8 +114,8 @@ void ads7952_read_all_channels(temperature_t * adc)
         }
         output_high(ADC2_SEL);
         
-        ch = (msb >> 4) + 12;
-        adc[ch].raw = ((0x0F & msb) << 8 ) | lsb;
+        ch = (msb >> 4);
+        adc[g_channel_map2[ch]].raw = ((0x0F & msb) << 8 ) | lsb;
     }
 }
 
